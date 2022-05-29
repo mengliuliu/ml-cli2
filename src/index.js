6e6073d1e00dfee0c83b2ad54d130a0eb52a808d
mengliu <mengliu@wxblockchain.com>
2022-05-29 15:02:33 +0800

    样式抽离+处理图片资源

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import _ from "lodash";
-import printMe from "./print.js";
+import Icon from "./icon.png";
+import "./index.css";
+import "./common.less";
 
 function component() {
   const element = document.createElement("div");
   const btn = document.createElement("button");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  
-  btn.innerHTML = "Click me and check the console!";
-  btn.onclick = printMe;
 
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = () => {
+    console.log("btn click");
+  };
+  element.classList.add("hello");
   element.appendChild(btn);
+
+  // 将图像添加到我们已经存在的 div 中。
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,79 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: {
-    index: "./src/index.js",
-    print: "./src/print.js",
-  },
+  entry: "./src/index.js",
   devtool: "inline-source-map",
-  devServer: {
-    // 将 dist 目录下的文件 serve 到 localhost:8080 下
-    static: "./dist",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "管理输出",
-    }),
-  ],
+  mode: "development",
   output: {
-    filename: "[name].bundle.js",
+    filename: "js/[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  optimization: {
-    runtimeChunk: "single",
+  module: {
+    rules: [
+      //层叠样式表抽离
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "less-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset",
+        generator: {
+          filename: "images/[name].[contenthash:6].[ext]",
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 4 * 1024, // 低于4kb转换成data URI
+          },
+        },
+      },
+      //本地资源,由于是客户端本地打包，图片资源不需要使用url-loader转为base64注入js bundle中
+      //   {
+      //     test: /\.(png|jpg|gif|jpeg)$/,
+      //     use: {
+      //       loader: "file-loader",
+      //       options: {
+      //         name: "images/[name].[contenthash:6].[ext]",
+      //         esModule: false, // file-loader 默认使用 ES6 模块解析，将其关闭，启用 CommonJS 模块，不配置这个，html 文件中的图片路径不对
+      //       },
+      //     },
+      //     type: "javascript/auto", // 不加这个配置，一张图片打包后会生成两张
+      //   },
+    ],
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "配置",
+    }),
+    //css抽取插件，用于将css文件从js中剥离成单独的css文件
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash:6].css",
+    }),
+  ],
+  //   devServer: {
+  //     // 将 dist 目录下的文件 serve 到 localhost:8080 下
+  //     // static: "./dist",
+  //   },
+  //   optimization: {
+  //     runtimeChunk: "single",
+  //   },
 };
