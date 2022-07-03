92da9434413fecbeb9ffa9bf7638829c7d2cfd0e
孟刘 <171626037@qq.com>
2022-07-03 16:20:14 +0800

    test: 测试hooks钩子

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 // import 'common.less';
-import './index.css';
+import './index.css'
 
-import App from './app';
-type name = 'meng' | 'liu';
-const name: name = 'meng';
-const n: any = 123;
-console.log(n);
-console.log(name);
+import App from './app'
+type name = 'meng' | 'liu'
+const name: name = 'meng'
+const n: any = 123
+console.log(n)
+console.log(name)
 
-const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const container = document.getElementById('root')
+const root = createRoot(container) // createRoot(container!) if you use TypeScript
 root.render(
     <div>
         hello world
         <App name="meng" age={24}></App>
     </div>,
-);
+)
