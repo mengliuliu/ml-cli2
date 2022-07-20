5c1b02e135d1398bae334dfaef3282c1cd695d6a
mengliu <171626037@qq.com>
2022-07-20 22:47:27 +0800

    ci: 开启打包分析

--- build/webpack/webpack.pro.config.js
+++ build/webpack/webpack.pro.config.js
@@ -3,6 +3,7 @@ const { merge } = require('webpack-merge')
 const CopyPlugin = require('copy-webpack-plugin')
 const TerserWebpackPlugin = require('terser-webpack-plugin')
 const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const BaseConfig = require('./webpack.base.config')
 
 module.exports = merge(BaseConfig, {
@@ -28,6 +29,11 @@ module.exports = merge(BaseConfig, {
                 },
             }),
             new CssMinimizerWebpackPlugin(),
+            new BundleAnalyzerPlugin({
+                analyzerMode: 'server', // 开一个本地服务查看报告
+                analyzerHost: '127.0.0.1', // host 设置
+                analyzerPort: 8888, // 端口号设置
+            }),
         ],
         splitChunks: {
             chunks: 'all',
